22fd43cbfc6f34900dff7738aae9c5f970227dab
Jesper Saastamoinen <Yeahsper@gmail.com>
2020-01-24 15:03:35 +0100

    All done ready for turn in

--- Battleship_With_Microserver/src/main/java/battleships/micro/battleshipsmicro/rest/FaqEndpoint.java
+++ Battleship_With_Microserver/src/main/java/battleships/micro/battleshipsmicro/rest/FaqEndpoint.java
@@ -12,11 +12,18 @@ public class FaqEndpoint {
 	@GET
     @Produces("text/plain")
     public Response doGet() {
-    	return Response.ok("Welcome to my program. To run the microservices, you need to\n" + 
+    	return Response.ok("Welcome to my Battleships. To run the microservices, you need to\n" + 
     						" start the microservice by choosing \"Run as Maven Build\"\n"+
     						" and with the goal as \"thorntail:run\". When it says \"Thorntail is Ready\" in the console\n"+
     						" you can enter through localhost:8080. \n\n"+
     						" To get a (pseudo(as all random is)) random generated battleship board\n"+
-    						" run Main.java which connects to localhost:8080/battleship").build();
+    						" run Main.java which connects to localhost:8080/battleship\n\n\n"+
+    						
+    						
+    						
+    						"Or you can run the 'battleships.micro-thorntail.jar' in a terminal as BASH or CMD \n"+
+    						" and then shut it down by aborting (ctrl+c).\n"+
+    						" If you run it in a IDE, remember to completely kill the process in the task manager to \n"+
+    						" shut down the service.").build();
     }
 }
